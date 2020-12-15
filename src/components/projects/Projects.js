91015b49f914f13d188cff7516fce43f8c091977
J Fav <jfavorito@outlook.com>
2020-12-15 10:43:09 -0700

    modified project page

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
         <div className="col-sm-2 "></div>
         <h5 className="col-sm-2 head-item">Link</h5>
         <h5 className="col-sm-2 head-item">Description</h5>
-        <h5 className="col-sm-2 head-item">Role</h5>
+        <h5 className="col-sm-2 head-item">Tech</h5>
         <div className="col-sm-2"></div>
       </div>
       {projects.map(project => (
@@ -21,7 +21,7 @@ function Projects() {
           <div className="col-sm-2 "><img className="project-img" alt={project.name} src={process.env.PUBLIC_URL + project.image} height="100px" width="100px" /></div>
           <p className="col-sm-2 project-item"><a href={project.link} className="project-link">{project.name}</a></p>
           <p className="col-sm-2 project-item">{project.description}</p>
-          <p className="col-sm-2 project-item">{project.role}</p>
+          <p className="col-sm-2 project-item">{project.tech}</p>
           <div className="col-sm-2"></div>
         </div>
       ))}
