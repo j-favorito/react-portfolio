bb9cc155b582879b717546a3523d82be4df7987c
J Fav <jfavorito@outlook.com>
2020-12-16 22:26:07 -0700

    final touches

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -17,10 +17,10 @@ function Projects() {
       </Row>
       {projects.map(project => (
         <Row className="row project-card" key={project.id}>
-          <Col md="3"><img className="project-img" alt={project.name} src={process.env.PUBLIC_URL + project.image} height="100px" width="200px" /></Col>
+          <Col md="3"><img className="project-img" alt={project.name} src={process.env.PUBLIC_URL + project.image} /></Col>
           <Col md="3" className="project-item"><a href={project.link} className="project-link">{project.name}</a></Col>
           <Col md="3" className="project-item">{project.description}</Col>
-          <Col md="3" className="project-item">{project.tech}</Col>
+          <Col md="3" className="project-item project-tech">{project.tech}</Col>
         </Row>
       ))}
       </Container>
