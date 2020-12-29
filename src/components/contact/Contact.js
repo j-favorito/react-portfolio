bcb24a4717c827fe94173a4df9083784697486fa
J Fav <jfavorito@outlook.com>
2020-12-29 10:13:27 -0700

    added rel=noreferrer and added gmail link

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -10,9 +10,9 @@ function Contact() {
           <Row className="contact-card">
             <Col xl="1"></Col>
             <Col xl="2" className="contact-item"><strong>Cell Phone: </strong><br></br>612-558-6610</Col>
-            <Col xl="4" className="contact-item"><strong>Email: </strong><br></br>joefavorito.work@gmail.com</Col>
-            <Col xl="2" className="contact-item"><strong>LinkedIn: </strong><br></br><a className="contact-link" href="https://www.linkedin.com/in/joseph-favorito-1332481a1">Joseph Favorito</a></Col>
-            <Col xl="2" className="contact-item"><strong>GitHub: </strong><br></br><a className="contact-link" href="https://github.com/j-favorito">j-favorito</a></Col>
+            <Col xl="4" className="contact-item"><strong>Email: </strong><br></br><a className="contact-link" href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=joefavorito.work@gmail.com" target="_blank" rel="noreferrer">joefavorito.work@gmail.com</a></Col>
+            <Col xl="2" className="contact-item"><strong>LinkedIn: </strong><br></br><a className="contact-link" href="https://www.linkedin.com/in/joseph-favorito-1332481a1" target="_blank" rel="noreferrer">Joseph Favorito</a></Col>
+            <Col xl="2" className="contact-item"><strong>GitHub: </strong><br></br><a className="contact-link" href="https://github.com/j-favorito" target="_blank" rel="noreferrer">j-favorito</a></Col>
             <Col xl="1"></Col>
           </Row>
         </Container>
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -12,7 +12,7 @@ function Projects() {
       {projects.map(project => (
         <Row className="row project-card" key={project.id}>
           <Col md="3"><img className="project-img" alt={project.name} src={process.env.PUBLIC_URL + project.image} /></Col>
-          <Col md="3" className="project-item"><a href={project.link} className="project-link" target="_blank">{project.name}</a></Col>
+          <Col md="3" className="project-item"><a href={project.link} className="project-link" target="_blank" rel="noreferrer">{project.name}</a></Col>
           <Col md="3" className="project-item">{project.description}</Col>
           <Col md="3" className="project-item project-tech">{project.tech}</Col>
         </Row>
